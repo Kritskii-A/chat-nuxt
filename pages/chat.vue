9fc69de3ec184fc2d93f38ba1c4d87004cf28920
Kritskii-A <kritskii-a@bk.ru>
2020-12-07 17:46:41 +0300

    title

--- pages/chat.vue
+++ pages/chat.vue
@@ -6,6 +6,11 @@
 import { mapState } from "vuex";
 export default {
   middleware: ["chat"],
+  head() {
+    return {
+      title: `Комната - ${this.user.room}`
+    };
+  },
   computed: mapState(["user"])
 };
 </script>
