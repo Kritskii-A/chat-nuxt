2305e49ca9e4c439e7bb70eb917870ac21ef7679
Kritskii-A <kritskii-a@bk.ru>
2020-12-08 15:48:27 +0300

    get messages

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const app = require("express")();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
+const users = require("./users")();
 
 const m = (name, text, id) => ({ name, text, id });
 
@@ -15,6 +16,14 @@ io.on("connection", socket => {
     }
 
     socket.join(data.room);
+
+    users.remove(socket.id); // удаляем на всякий случай
+    // добавляем пользователя
+    users.add({
+      id: socket.id,
+      name: data.name,
+      room: data.room
+    });
     // если все ок, то возвращаем объект данных
     cb({ userId: socket.id });
     // отправка сообщения пользователю при входе в комнату
@@ -28,12 +37,16 @@ io.on("connection", socket => {
       );
   });
 
-  socket.on("createMessage", data => {
-    setTimeout(() => {
-      socket.emit("newMessage", {
-        text: data.text + " SERVER"
-      });
-    }, 500);
+  socket.on("createMessage", (data, cb) => {
+    if (!data.text) {
+      return cb("Текст не может быть пустым");
+    }
+
+    const user = users.get(data.id);
+    if (user) {
+      io.to(user.room).emit("newMessage", m(user.name, data.text, data.id));
+    }
+    cb();
   });
 });
 
